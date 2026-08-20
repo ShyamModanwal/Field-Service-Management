48be0eb655b067cfb49bf9bf54d5fedb6c35e98b
Shyam Modanwal <shyammodanwal705@gmail.com>
2026-08-20 20:39:25 +0530

    Fix backend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,21 +41,20 @@ function App() {
     try {
 
       const response = await fetch(
-        "https://confident-ambition-production-7bdb.up.railway.app/api/auth/login",
-        {
-          method: "POST",
+  "https://confident-ambition-production-7bdb.up.railway.app/api/auth/login",
+  {
+    method: "POST",
 
-          headers: {
-            "Content-Type": "application/json"
-          },
+    headers: {
+      "Content-Type": "application/json"
+    },
 
-          body: JSON.stringify({
-            email: email,
-            password: password
-          })
-        }
-      );
-
+    body: JSON.stringify({
+      email: email,
+      password: password
+    })
+  }
+);
 
       const data = await response.json();
 
